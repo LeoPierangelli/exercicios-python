af2ddf44afad7b19ea4a3ad8b4367b5140babd9c
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2024-09-04 11:44:41 -0300

    a
    a

--- ex04.py
+++ ex04.py
@@ -1,7 +1,14 @@
-a = int(input('Quantas maçãs vc comprou?\n-> '))
+'''a = int(input('Quantas maçãs vc comprou?\n-> '))
 if a>=12:
     a = a*0.30
     print(f'você gastou R${a}')
 else:
     a = a*0.25
-    print(f' você gastou R${a}')
+    print(f'você gastou R${a}')'''
+
+#gabarito:
+a = int(input('Quantas maçãs vc comprou?\n-> '))
+p = 0.25
+if a>=12:
+    p = 0.30
+print(f'você gastou R${p*a}')
