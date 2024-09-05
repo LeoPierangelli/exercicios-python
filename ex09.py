6547a18e327e64af6097094c398a67c9f404cbdc
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2024-09-05 04:58:57 -0300

    ex09 completo

---
+++ ex09.py
@@ -0,0 +1,13 @@
+a = int(input('Digite um valor: '))
+b = int(input('Digite outro valor: '))
+c = int(input('Digite o último valor: '))
+
+if a>b and a>c:
+    print(f'{a}')
+elif b>c and b>a:
+    print(f'{b}')
+else:
+    print(f'{c}')
+
+
+
