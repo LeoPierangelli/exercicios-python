868bc096c5d68cbb247dfe7c1f5d449c45fc7a1a
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2024-09-05 04:40:03 -0300

    ex08 completo

---
+++ ex08.py
@@ -0,0 +1,15 @@
+s = int(input('Digite o número de lados do polígono: '))
+l = float(input('Digite a medida do lado em cm: '))
+
+if s<3:
+    print('Não é um polígono.')
+elif s==3:
+    a = (l**2 * 1**1/3)/2
+    print(f'Triângulo de área igual a {a:.2f}')
+elif s==4:
+    a = l**2
+    print(f'Quadrado de área igual a {a:.2f}')
+elif s==5:
+    print('Pentágono')
+else:
+    print('Polígono não identificado')
