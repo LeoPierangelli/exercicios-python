b6dc69964bbfb0c35563fca6d03aee89e56329f3
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2024-09-11 14:31:31 -0300

    ex multas melhor

--- ex multas.py
+++ ex multas.py
@@ -1,11 +1,13 @@
 v = int(input('Digite a velocidade em km/h: '))
 
+if v < 80:
+    print('não há multa')
+else:
+    if v >= 80 and v <= 100:
+        multa = v*1.2
+    elif v > 100 and v <= 120:
+        multa = 100*1.2 + v*1.3
+    elif v > 120:
+        multa = 100*1.2 + 120*1.3 + v*1.4
 
-if v>= 80 and v <= 100:
-    multa = 100*1.2
-elif v>= 100 and v<= 120:
-    multa = 100*1.2 + v*1.3
-elif v>= 120 and v<=150:
-    multa = 100*1.2 + 120*1.3 + v*1.4
-
-print(f'valor da multa: R${multa:.2f}')
+    print(f'valor da multa: R${multa:.2f}')
